58d22f863955689ae1242c1b0503791daa227027
Justin-Insight <jparsons@insightcreative.com>
2020-01-23 13:28:42 -0600

    updated path to the favicon

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link href="/wp-content/themes/insightcustom/favicon.png" rel="shortcut icon">
+	<link href="/wp-content/themes/insightcustom/images/favicon.png" rel="shortcut icon">
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-13074741-55"></script>
